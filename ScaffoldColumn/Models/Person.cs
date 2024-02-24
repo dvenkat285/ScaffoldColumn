196b42b297f4a209164033f7f5be685406d98789
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-02-24 22:00:31 +0530

    Add project files.

---
+++ ScaffoldColumn/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScaffoldColumn.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string age { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string contact { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string address { get; set; }
+
+
+    }
+}
